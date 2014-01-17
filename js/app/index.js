9bc2f79375d314e9f903931827ff207551b84cde
web-intelligence <info@webintelligence.ie>
2014-01-17 17:40:58 +0000

    third commit

--- js/app/index.js
+++ js/app/index.js
@@ -24,42 +24,26 @@ var app = {
             
   
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
 
         document.addEventListener('deviceready', this.onDeviceReady, false);
-       // document.addEventListener('deviceready', this.registerDevice, false);
     },
      
-    
-    stackTrace: function(){
-      
-          var err = new Error();
-            return err.stack;
-        
-    },
     
     /*
      * This function registers the device with the server, and stores the device id and the api key.
      * This should only ever execute once. 
      */
     registerDeviceWithServer: function(e){
-
             
             require(["app/models/device"], function (model) {
-
-                    console.log('******in require******');
 
                     var deviceModel = new model.Device();
                     var deviceDetails = [];
 
                     deviceDetails.project_title = 'mountmercy';
                     deviceDetails.platform = window.device.platform;
-                    //deviceDetails.platform = 'android';
-
-                    console.log('******before the fucking save******');
 
                     deviceModel.save(deviceDetails, 
                         {                                    
@@ -67,18 +51,15 @@ var app = {
                         headers :{device_id:"63843",
                         api_key:"hv7Vgd4jsbb"},
                         success: function (data) {
-                            console.log('successfuly registered device');
                             var device_id = data.id;
                             var api_key = data.get('api_key');
                             window.localStorage.setItem('mountmercy_device_id', device_id);
                             window.localStorage.setItem('mountmercy_api_key', api_key);
+                            
                             //now update the Reg Id
-                            console.log('about to update...');
                             app.updateRegId(device_id, api_key, e);
                         },
                         error:   function(model, xhr, options){
-                           console.log('registerDeviceWithServer failed save, responseText is : ');
-                           console.log(xhr.responseText);
   
                         },
                     });
@@ -89,10 +70,7 @@ var app = {
     
     updateRegId: function(device_id, api_key, e){
         
-            console.log('in updateRegId');
-        
             if(typeof(device_id)!=='undefined' && device_id!==null){
-            //so already have device_id, therefore update the reg_id
             
                 require(["app/models/device"], function (model) {
 
@@ -101,20 +79,18 @@ var app = {
 
                         deviceDetails.reg_id = e.regid;
 
-                        console.log('before update');
                         deviceModel.save(deviceDetails, 
                             {                                    
                             api: true,
                             headers :{device_id:device_id,
                             api_key:api_key},
                             success: function (data) {
-                               // alert('in the success, registerd reg id');   
-                               console.log('sucessfully updated Reg Id');
+     
                             },
                             error:   function(model, xhr, options){
-                              // alert('in Error');
-                               console.log('failed to update, details: ');
-                               console.log(xhr.responseText);
+                                // alert('in Error');
+                                //console.log('failed to update, details: ');
+                                //console.log(xhr.responseText);
                             },
                         });
 
@@ -130,9 +106,6 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 
-        //app.registerDeviceWithServer();
-      
-      
         var pushNotification = window.plugins.pushNotification;
         if (window.device.platform == 'android' || window.device.platform == 'Android') {
             pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"475226855592","ecb":"app.onNotificationGCM"});                        
@@ -144,19 +117,7 @@ var app = {
 
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        /*
-    }
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);*/
-    },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
        //alert('Callback Success! Result = '+result)
@@ -170,7 +131,7 @@ var app = {
      * 
      * For iOS
      */        
-    tokenHandler:function(msg) {
+    tokenHandler:function(status) {
         
                     require(["app/models/device"], function (model) {
                         
@@ -178,12 +139,10 @@ var app = {
                             var deviceModel = new model.Device();
                             var deviceDetails = [];
 
-                            deviceDetails.reg_id = e.regid;
-                            //deviceDetails.reg_id = 'saaf7afadfhkadfh79dasdkh';
+                            deviceDetails.reg_id = status.deviceToken;
                             deviceDetails.project_title = 'mountmercy';
                             deviceDetails.platform = 'ios';
-
-                      
+                
                             deviceModel.save(deviceDetails, 
                                 {                                    
                                 api: true,
@@ -203,57 +162,11 @@ var app = {
                     });
     },
     
-    
-    logObject: function(obj){
-        
-        var ind = "";
-        if (arguments.length > 1)
-        {
-            ind = arguments[1];
-        }
-
-        if (typeof obj == "undefined" || obj == null)
-        {
-            console.log("<null>");
-            return;
-        }
-
-        if (typeof obj != "object")
-        {
-            console.log(obj);
-            return;
-        }
-
-        for (var key in obj)
-        {
-            if (typeof obj[key] == "object")
-            {
-                console.log(ind + key + "={");
-                this.logObject(obj[key], ind + "  ");
-                console.log(ind + "}");
-            }
-            else
-            {
-                console.log(ind + key + "=" + obj[key]);
-            }
-        }
-        
-    },
-            
+      
     /*
      * For Android Phones
      */
     onNotificationGCM: function(e) {
-     
-        //var news = new model.NewsCollection();
-
-       /* news.fetch({
-            full_url: false,
-            success: function (collection) {
-                that.body.removeClass('left-nav');
-                slider.slidePage(new NewsList({collection: collection}).$el);                          
-            }
-        });*/
                     
         switch( e.event )
         {
@@ -269,8 +182,7 @@ var app = {
                     if(typeof(device_id)==='undefined' || device_id===null){
                         //we dont have a device id so register it and save to local storage. 
                         //should only ever enter here once     
-                        
-                        console.log('in the if');
+    
                         this.registerDeviceWithServer(e);                      
                     }
                     else{
@@ -280,23 +192,13 @@ var app = {
                         
                     }
                     
-
-                    
                 }
                 break;
 
             case 'message':
-                // this is the actual push notification. its format depends on the data model from the push server
-                //alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-                
-                console.log('e is ');
-                console.log(this.logObject(e));
-                console.log('************* e.payload.id is ************');
-                console.log(e.payload.article_id);
-
+         
                 window.location.hash = "article/"+e.payload.article_id;
          
- 
                 break;
 
             case 'error':
